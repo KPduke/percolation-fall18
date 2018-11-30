5a9ac1773f29f561c699dbbf00f926874cc330aa
Karly Pearson <karly.pearson@duke.edu>
2018-11-29 21:13:47 -0500

    updatee

--- src/PercolationDFSFast.java
+++ src/PercolationDFSFast.java
@@ -12,22 +12,19 @@ public class PercolationDFSFast extends PercolationDFS {
 					String.format("(%d,%d) not in bounds", row,col));
 		}
 		if(row == 0){
-			myGrid[row][col] = FULL;	
+			dfs(row,col);
 		}
 		else if(inBounds(row-1, col) && isFull(row -1, col)){
-			myGrid[row][col] = FULL;
+			dfs(row,col);
 		}
 		else if(inBounds(row+1, col) && isFull(row+1, col)) {
-			myGrid[row][col] = FULL;
+			dfs(row,col);
 		}
 		else if(inBounds(row, col+1) && isFull(row, col+1)) {
-			myGrid[row][col] = FULL;
+			dfs(row,col);
 		}
 		else if( inBounds(row, col -1) && isFull(row, col - 1)) {
-				myGrid[row][col] = FULL;
-		}
-		if (myGrid[row][col] == FULL) {
-			dfs(row, col);
+			dfs(row,col);
 		}
 		
 	} 
