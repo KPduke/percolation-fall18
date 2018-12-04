985e791c47440208ea6ef5f00af08eb50be9c5af
Karly Pearson <karly.pearson@duke.edu>
2018-12-03 21:45:52 -0500

    changed

--- src/PercolationUF.java
+++ src/PercolationUF.java
@@ -6,16 +6,14 @@ public class PercolationUF implements IPercolate {
 	private final int VBOTTOM;
 	private final QuickUWPC myFinder; 
 	private final int[][] myGrid; 
-	
 
 	public PercolationUF (int n, IUnionFind finder) {
 		finder.initialize(n*n + 2); 
-		myFinder = (QuickUWPC)finder;
+		myFinder = finder;
 		VTOP = n*n;
 		VBOTTOM = n*n + 1;
 		myGrid = new int[n][n];
 		myOpenCount = 0;
-		
 	}
 	@Override
 	public void open(int row, int col) {
@@ -27,8 +25,17 @@ public class PercolationUF implements IPercolate {
 		if(myGrid[row][col] == OPEN) {
 			myFinder.union(row,col);
 		}
-		if(myGrid[row+1][col] == OPEN) {
+		if(myGrid[row+1][col] == OPEN && inBounds(row+1, col)) {
 			myFinder.union(helper(row+1, col), helper(row, col));
+		}
+		if(myGrid[row][col+1] == OPEN && inBounds(row, col+1)) {
+			myFinder.union(helper(row, col+1), helper(row, col));
+		}
+		if(myGrid[row-1][col] == OPEN && inBounds(row-1,col)) {
+			myFinder.union(helper(row-1, col), helper(row, col));
+		}
+		if(myGrid[row][col-1] == OPEN && inBounds(row,col-1)) {
+			myFinder.union(helper(row, col-1), helper(row, col));
 		}
 	}
 	public int helper(int row, int col) {
@@ -36,18 +43,14 @@ public class PercolationUF implements IPercolate {
 		int val = row*size + col;
 		return val;
 	}
-	
 	@Override
 	public boolean isOpen(int i, int j) {
-		
 		if(!inBounds(i,j)) {
 			throw new IndexOutOfBoundsException (
 					String.format("(%d,%d) not in bounds", i,j));
 		}
 		return myGrid[i][j] == OPEN; 
-		
 	}
-	
 	@Override
 	public boolean isFull(int i, int j) {
 		if (! inBounds(i,j)) {
@@ -59,16 +62,13 @@ public class PercolationUF implements IPercolate {
 		}
 			return false;
 	}
-		
 	@Override
 	public boolean percolates() {
 		return myFinder.connected(VTOP, VBOTTOM);
 	}
-	
 	@Override
 	public int numberOfOpenSites() {
 		return myOpenCount;
-		
 	}
 	protected boolean inBounds(int row, int col) {
 		if (row < 0 || row >= myGrid.length) return false;
