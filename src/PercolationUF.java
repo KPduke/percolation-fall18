14f58cdc984e42e3b06d9b7a12547056b5c49153
Karly Pearson <karly.pearson@duke.edu>
2018-12-03 21:50:11 -0500

    updatedUFF

--- src/PercolationUF.java
+++ src/PercolationUF.java
@@ -4,12 +4,12 @@ public class PercolationUF implements IPercolate {
 	private int myOpenCount;
 	private final int VTOP;
 	private final int VBOTTOM;
-	private final QuickUWPC myFinder; 
+	private final IUnionFind myFinder; 
 	private final int[][] myGrid; 
 
 	public PercolationUF (int n, IUnionFind finder) {
 		finder.initialize(n*n + 2); 
-		myFinder = finder;
+		myFinder = (QuickUWPC) finder;
 		VTOP = n*n;
 		VBOTTOM = n*n + 1;
 		myGrid = new int[n][n];
@@ -25,16 +25,16 @@ public class PercolationUF implements IPercolate {
 		if(myGrid[row][col] == OPEN) {
 			myFinder.union(row,col);
 		}
-		if(myGrid[row+1][col] == OPEN && inBounds(row+1, col)) {
+		if(inBounds(row+1, col) && myGrid[row+1][col] == OPEN ) {
 			myFinder.union(helper(row+1, col), helper(row, col));
 		}
-		if(myGrid[row][col+1] == OPEN && inBounds(row, col+1)) {
+		if(inBounds(row, col+1)&& myGrid[row][col+1] == OPEN ) {
 			myFinder.union(helper(row, col+1), helper(row, col));
 		}
-		if(myGrid[row-1][col] == OPEN && inBounds(row-1,col)) {
+		if(inBounds(row-1,col) && myGrid[row-1][col] == OPEN) {
 			myFinder.union(helper(row-1, col), helper(row, col));
 		}
-		if(myGrid[row][col-1] == OPEN && inBounds(row,col-1)) {
+		if(inBounds(row,col-1) && myGrid[row][col-1] == OPEN) {
 			myFinder.union(helper(row, col-1), helper(row, col));
 		}
 	}
