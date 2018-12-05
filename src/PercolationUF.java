3d131b07c20bb268a530888894a537028502e418
Karly Pearson <karly.pearson@duke.edu>
2018-12-04 20:49:53 -0500

    pushUFchange

--- src/PercolationUF.java
+++ src/PercolationUF.java
@@ -5,14 +5,14 @@ public class PercolationUF implements IPercolate {
 	private final int VTOP;
 	private final int VBOTTOM;
 	private final IUnionFind myFinder; 
-	private final int[][] myGrid; 
+	private final boolean [][]myGrid; 
 
 	public PercolationUF (int n, IUnionFind finder) {
 		finder.initialize(n*n + 2); 
 		myFinder = finder;
 		VTOP = n*n;
 		VBOTTOM = n*n + 1;
-		myGrid = new int[n][n];
+		myGrid = new boolean[n][n];
 		myOpenCount = 0;
 	}
 	@Override
@@ -21,20 +21,29 @@ public class PercolationUF implements IPercolate {
 			throw new IndexOutOfBoundsException (
 					String.format("(%d,%d) not in bounds", row,col));
 		}
-		myOpenCount ++;
-		if(myGrid[row][col] == OPEN) {
-			myFinder.union(row,col);
+		if(myGrid[row][col] == true) {
+			return;
 		}
-		if(inBounds(row+1, col) && myGrid[row+1][col] == OPEN ) {
+		myGrid[row][col] = true;
+		myOpenCount++;
+		
+		if(row == 0) {
+			myFinder.union(helper(row,col), VTOP);
+		}
+		if(row == myGrid.length - 1) {
+			myFinder.union(helper(row,col), VBOTTOM);
+		}
+		
+		if(inBounds(row+1, col) && myGrid[row+1][col] == true) {
 			myFinder.union(helper(row+1, col), helper(row, col));
 		}
-		if(inBounds(row, col+1)&& myGrid[row][col+1] == OPEN ) {
+		if(inBounds(row, col+1)&& myGrid[row][col+1] == true ) {
 			myFinder.union(helper(row, col+1), helper(row, col));
 		}
-		if(inBounds(row-1,col) && myGrid[row-1][col] == OPEN) {
+		if(inBounds(row-1,col) && myGrid[row-1][col] == true) {
 			myFinder.union(helper(row-1, col), helper(row, col));
 		}
-		if(inBounds(row,col-1) && myGrid[row][col-1] == OPEN) {
+		if(inBounds(row,col-1) && myGrid[row][col-1] == true) {
 			myFinder.union(helper(row, col-1), helper(row, col));
 		}
 	}
@@ -49,7 +58,7 @@ public class PercolationUF implements IPercolate {
 			throw new IndexOutOfBoundsException (
 					String.format("(%d,%d) not in bounds", i,j));
 		}
-		return myGrid[i][j] == OPEN; 
+		return myGrid[i][j] == true; 
 	}
 	@Override
 	public boolean isFull(int i, int j) {
